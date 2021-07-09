9e48686d7d367ad41d669f63cd4e8c123994dfed
h4ck3r@parrot <77598345+parrothacker1@users.noreply.github.com>
2021-07-09 13:23:58 +0000

    Add files via upload

--- newfile.java
+++ newfile.java
@@ -1,35 +1,28 @@
-// To check if a number is repeated within a number 
-// Input: 54422
-// It is not a unique number
-
 import java.util.*;
-
+import java.lang.Math;
 public class Main {
 	public static void main(String[] args) {
-		int tot=0;
-		ArrayList<Character> obj=new ArrayList();
-		System.out.print("Input the number: ");
+		System.out.print("Input the value of a: ");
 		Scanner scan=new	Scanner(System.in);
-		int num=scan.nextInt();
-		String str_num=Integer.toString(num);
-		int len=str_num.length();
-		for (int i=0;i<len;i++) {
-			obj.add(str_num.charAt(i));
-		}
-		for (int i=0;i<len;i++) {
-			char a=str_num.charAt(i);
-			for (int j=0;j<obj.size();j++) {
-				if (a==obj.get(j)) {
-					tot=tot+1;
-				} else {
-					continue;
-				}
-			}
-		}
-		if (len==tot) {
-			System.out.println("It is a unique number");
+		int a=scan.nextInt();
+		System.out.print("Input the value of b: ");
+		int b=scan.nextInt();
+		System.out.print("Input the value of c: ");
+		int c=scan.nextInt();
+		int D=b*b-4*a*c;
+		System.out.println("The value of discriminant is "+D);
+		if (D>0) {
+			double d2=Math.sqrt(D);
+			int b2=0-b;
+			double x1 = (b2+d2)/2*a;
+			double x2= (b2-d2)/2*a;
+			System.out.println("The values of x are "+x1+" and "+x2);
+		} else if (D==0) {
+			int b2=0-b;
+			double x=(b2)/2*a;
+			System.out.println("The value of x is "+x);
 		} else {
-			System.out.println("It is not a unique number");
+			System.out.println("There are no real roots");
 		}
 	}
 }
